eda6fec4b0c9904f09185e0c61730253e643f0ce
Charles Amat <charlie.amat@force5solutions.com>
2017-08-03 14:08:55 -0400

    Removed some left over scratch code

--- Assets/Heart.cs
+++ Assets/Heart.cs
@@ -1,6 +1,4 @@
 using System;
-using UnityEditor;
-using UnityEngine;
 using UnityEngine.UI;
 
 public class Heart
@@ -29,47 +27,5 @@ public class Heart
     {
         if (numberOfHeartPieces < 0) throw new ArgumentOutOfRangeException("numberOfHeartPieces");
         _image.fillAmount -= numberOfHeartPieces * FillPerHeartPiece;
-    }
-
-    public float Speed;
-    public Transform transform;
-
-    private void Update1()
-    {
-        var h = Input.GetAxisRaw("Horizontal");
-        var v = Input.GetAxisRaw("Vertical");
-
-        var x = h * Speed * Time.deltaTime;
-        var z = v * Speed * Time.deltaTime;
-
-        transform.position += new Vector3(x, 0, z);
-    }
-
-    public Movement Movement;
-
-    private void Update2()
-    {
-        transform.position += Movement.GetNextPosition(
-            Input.GetAxisRaw("Horizontal"), 
-            Input.GetAxisRaw("Vertical"), 
-            Time.deltaTime);
-    }
-
-    public Vector3 GetPositionAfterMovement(float h, float v, float delta)
-    {
-        return new Vector3(h*Speed*delta, 0, v);
-    }
-}
-
-public class Movement
-{
-    public float Speed { get; set; }
-
-    public Vector3 GetNextPosition(float h, float v, float delta)
-    {
-        var x = h * Speed * Time.deltaTime;
-        var z = v * Speed * Time.deltaTime;
-
-        return new Vector3(h, 0, v);
     }
 }
